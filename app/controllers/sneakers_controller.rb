e138f4ac15e62544807b1c8e686f6274488c0e84
Lucas Froidefond <froidefond.lucas@gmail.com>
2022-04-05 14:21:34 +0200

    tt

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -28,7 +28,7 @@ class SneakersController < ApplicationController
   # POST /sneakers or /sneakers.json
   def create
     @sneaker = Sneaker.new(sneaker_params)
-    @sneaker.user_id = current_user.id
+    @post.user_id = current_user.id
     respond_to do |format|
       if @sneaker.save
         format.html { redirect_to sneaker_url(@sneaker), notice: "Sneaker was successfully created." }
