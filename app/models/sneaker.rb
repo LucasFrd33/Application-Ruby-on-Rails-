28e139f83436761763f556ac73854bd8611daf31
Lucas Froidefond <froidefond.lucas@gmail.com>
2022-04-05 18:26:09 +0200

    tt

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,3 +1,3 @@
 class Sneaker < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user, optional: true, class_name: 'User', foreign_key: 'user_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :sneakers
+
+  has_many :sneakers, foreign_key: 'user_id', dependent: :destroy
 end
