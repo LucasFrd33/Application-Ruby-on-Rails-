678f565f2ee58198dfa020585121ed2b962fa55b
Lucas Froidefond <froidefond.lucas@gmail.com>
2022-04-05 18:08:32 +0200

    tt

--- db/schema.rb
+++ db/schema.rb
@@ -27,13 +27,6 @@ ActiveRecord::Schema.define(version: 2022_04_04_150048) do
     t.integer "user_id"
   end
 
-  create_table "stories", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_04_04_150048) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.integer "user_id"
   end
 
 end
